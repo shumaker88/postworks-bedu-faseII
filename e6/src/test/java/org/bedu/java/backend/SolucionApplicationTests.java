3453b82b514153edb20442dab0b453338e66c90b
ivangarcia1296 <homo1864@hotmail.com>
2023-03-24 15:29:26 -0600

    Se anhade postwork 6

---
+++ e6/src/test/java/org/bedu/java/backend/SolucionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.java.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolucionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
