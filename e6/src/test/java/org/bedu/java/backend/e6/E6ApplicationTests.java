3627746b6eda96a944ecf6571dfaf90804abb0ee
Victor Sanchez <soporteaplicaciones@innovasport.com>
2023-03-25 12:57:17 -0600

    Se agregan archivos

---
+++ e6/src/test/java/org/bedu/java/backend/e6/E6ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.java.backend.e6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ e8/src/test/java/org/bedu/java/backend/e8/E8ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.java.backend.e8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
